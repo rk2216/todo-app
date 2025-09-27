f5c63766d15b3ffc2cc079cd884f4a9f2fe94f27
rk2216 <rockeshkumawath26@gmail.com>
2025-09-27 08:47:25 +0530

    added input validations

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {todo, updateTodo} = require("/types");
 
 const app = express();
 const PORT = 3000;
@@ -10,11 +11,27 @@ app.get("/todos", (req, res) => {
 });
 
 app.post("/todos", (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = todo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent wrong inputs",
+        });
+        return;
+    }
 
 });
 
 app.put("/completed", (req, res) => {
-
+    const updatePayload = req.body;
+    const parsedPayload = todo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent wrong inputs",
+        });
+        return;
+    }
+    
 });
 
 app.listen(PORT, (err) => {
