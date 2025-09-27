5e5d274a1f8267f8188b26c9683301d83fb469c2
rk2216 <rockeshkumawath26@gmail.com>
2025-09-27 09:29:23 +0530

    Bug fix for udpate db call

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.put("/completed", async (req, res) => {
         return;
     }
 
-    await todo.update({
+    await todo.updateOne({
         _id: updatePayload.id
     }, {
         completed: true
